fbb9621b462be39a16487ded733647b074ab6c4c
William Ku <kucheech@gmail.com>
2021-12-28 10:39:27 +0800

    add stopBallIfOutOfBounds() to prevent ball from going out of the field

--- app.js
+++ app.js
@@ -1,9 +1,29 @@
 /* eslint-disable indent */
 /* eslint-disable no-undef */
+
+function stopBallIfOutOfBounds() {
+  const $ball = $('#ball');
+  const { left, top } = $ball.position();
+  console.log(left, top);
+  if (top < 20) {
+    $ball.css('top', 0);
+  }
+  if (left < 80) {
+    $ball.css('left', 0);
+  }
+  if (left > 1180) {
+    $ball.css('left', 1110);
+  }
+  if (top > 600) {
+    $ball.css('top', 580);
+  }
+}
+
 function moveBall(x, y) {
   const $ball = $('#ball');
   $ball.css('left', `+=${x}`);
   $ball.css('top', `+=${y}`);
+  stopBallIfOutOfBounds();
 }
 
 $(() => {
