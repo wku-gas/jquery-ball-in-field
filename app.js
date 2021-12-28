15740c0f2d032a2357bf9e94014d73e2a4a8d6e5
William Ku <kucheech@gmail.com>
2021-12-28 10:15:11 +0800

    add keydown handler to move ball using arrow buttons

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
+function moveBall(x, y) {
+  const $ball = $("#ball")
+  $ball.css("left", `+=${x}`)
+  $ball.css("top", `+=${y}`)
+}
+
 $(() => {
-  console.log($("#ball").position());
+  const $ball = $("#ball")
+
+  $("body").keydown(function (e) {
+    switch (e.keyCode) {
+      case 37: moveBall(-10, 0); break;
+      case 38: moveBall(0, -10); break;
+      case 39: moveBall(10, 0); break;
+      case 40: moveBall(0, 10); break;
+    }
+  })
 });
