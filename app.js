ef65f2fad9fbfd633aa5fb6a5c44d125ee16daa4
William Ku <kucheech@gmail.com>
2021-12-28 10:20:18 +0800

    add eslint

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
+/* eslint-disable indent */
+/* eslint-disable no-undef */
 function moveBall(x, y) {
-  const $ball = $("#ball")
-  $ball.css("left", `+=${x}`)
-  $ball.css("top", `+=${y}`)
+  const $ball = $('#ball');
+  $ball.css('left', `+=${x}`);
+  $ball.css('top', `+=${y}`);
 }
 
 $(() => {
-  const $ball = $("#ball")
-
-  $("body").keydown(function (e) {
+  $('body').keydown(function (e) {
     switch (e.keyCode) {
       case 37: moveBall(-10, 0); break;
       case 38: moveBall(0, -10); break;
       case 39: moveBall(10, 0); break;
       case 40: moveBall(0, 10); break;
     }
-  })
+  });
 });
